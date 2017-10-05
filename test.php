cf77398502dcffc5358e6236256b84ff61a688ba
Etudiant ESIPE <Etudiant ESIPE>
2017-10-05 12:39:39 +0200

    creation de conflit

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php $file="Rates.csv";
 $csv= file_get_contents($file);
-$array = array_map("str_getcsv", explode("\n", $csv));
+$array = array_map("str_getcsv", explode("\n", $csv)); // Ceci est un test
 $json = json_encode($array);
 print_r($json); ?>
