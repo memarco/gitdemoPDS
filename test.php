fe1e235d12522d801bc076f3a5a82dd06a678364
Etudiant ESIPE <Etudiant ESIPE>
2017-10-05 13:06:50 +0200

    Deuxième test

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php $file="Rates.csv";
 $csv= file_get_contents($file); 
-$array = array_map("str_getcsv", explode("\n", $csv));//blablacar Ceci est un test
+$array = array_map("str_getcsv", explode("\n", $csv));//blablacar Ceci est un test sur GUI
 $json = json_encode($array);
 print_r($json); ?>
